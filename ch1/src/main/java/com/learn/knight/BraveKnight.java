c016c60792ac2b6319c3df2f6b7c1c0f4979f33d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-23 09:25:03 -0700

    Add class BraveKnight

---
+++ ch1/src/main/java/com/learn/knight/BraveKnight.java
@@ -0,0 +1,10 @@
+package com.learn.knight;
+
+public class BraveKnight implements Knight {
+    private Quest quest;
+
+    public BraveKnight(Quest quest) {
+        this.quest = quest;
+    }
+
+}
