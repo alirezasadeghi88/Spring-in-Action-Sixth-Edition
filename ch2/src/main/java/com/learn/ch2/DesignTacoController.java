f2af5c876f40fff86adee111f706c36e3a2b3793
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-26 06:19:06 -0700

    add class TacoOrder

---
+++ ch2/src/main/java/com/learn/ch2/DesignTacoController.java
@@ -0,0 +1,13 @@
+package com.learn.ch2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("tacoOrder")
+public class DesignTacoController {
+}
