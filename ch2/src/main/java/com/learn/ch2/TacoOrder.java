f966de68324d4dd99b22260e0b2bf69368d472fe
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-26 06:14:59 -0700

    add class TacoOrder

--- ch2/src/main/java/com/learn/ch2/TacoOrder.java
+++ ch2/src/main/java/com/learn/ch2/TacoOrder.java
@@ -4,5 +4,12 @@ import lombok.Data;
 
 @Data
 public class TacoOrder {
-
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
 }
